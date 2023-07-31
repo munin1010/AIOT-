f151994e7dbba67c2771f7fe051f5d9e9b71bb93
b36660407 <135082868+b36660407@users.noreply.github.com>
2023-07-31 17:41:57 +0800

    Update 氣壓感測器+OLED.py

--- 氣壓感測器/氣壓感測器+OLED.py
+++ 氣壓感測器/氣壓感測器+OLED.py
@@ -1,23 +1,28 @@
-from machine import ADC,Pin,I2C,UART
-from ssd1306 import SSD1306_I2C
-import utime
+from machine import ADC,Pin
+import time
 import math
+from machine import Pin,I2C,UART,ADC
+from ssd1306 import SSD1306_I2C
 
-sda=machine.Pin(12) #OLED
-adc = ADC(Pin(28))  #感測器
-i2c=I2C(0,sda=Pin(12),scl=Pin(28),freq=400000)
-oled=SSD1306_I2C(128,64,i2c)
-oled.fill(0)#0 全暗 1全亮
+i2c= I2C(0,sda= Pin(12),scl=Pin(13),freq= 400000)
+oled= SSD1306_I2C(128,64,i2c)
+oled.fill(0)
+
+
+
+adc = ADC(Pin(26))  
 conversion_factor=5/65535    
-   
+
 
 while 1:
     reading=adc.read_u16()*conversion_factor
-    print(reading)
-    utime.sleep(0.5)
-    
-    disp=str(reading)
-    f.text(disp,10,12,32)#X Y座標  字會在那顯示
-    f.show()
+    pressure = round(reading*10,1)
+    print(pressure)
     time.sleep(0.5)
-    oled.fill(0)#清掉  所則字會重疊
+    
+    disp = str(pressure)
+    oled.text(disp,10,12)
+    oled.show()
+    time.sleep(0.5)
+    oled.fill(0)
+oled.show()
