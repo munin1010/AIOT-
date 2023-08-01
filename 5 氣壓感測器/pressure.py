8b54fbfee7d9410ad794b0d7f75583cac97085f0
b36660407 <b36660407@gmail.com>
2023-08-01 15:15:28 +0800

    1111

--- 5 氣壓感測器/pressure.py
+++ 5 氣壓感測器/pressure.py
@@ -9,6 +9,7 @@ while 1:
     reading=adc.read_u16()*conversion_factor
     #
     pressure = round(reading*10,1)
+    print("pressure = {} hpa".format(pressure)) 
     y=str(pressure)
     #
     print(pressure)
