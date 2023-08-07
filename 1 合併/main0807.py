966fb1e553240c5826ddcad16cba5639a3488a76
Bruce <b36660407@gmail.com>
2023-08-07 17:06:50 +0800

    1

--- 1 合併/main0807.py
+++ 1 合併/main0807.py
@@ -1,7 +1,8 @@
 from MQTT2 import *
-from water_flow import *
+from water_flow_1 import *
 from pressure import *
 from waterlevel0804 import *
+from thingspeak0807 import *
 def setup():
     init_pressure_sensor()
     init_flow_sensor()
@@ -11,6 +12,11 @@ def loop():
     
     
     sensor={'pressure':0,'flow_rate':0,'water_level':0}
+    
+    wifi_module = WiFiModule()
+    wifi_module.connect_wifi("AndroidHuang", "0987437180", "1000000", "3ZVJK8QWCNL6E0AJ")
+
+        
     
     esp01_mqtt = ESP01_MQTT(tx_pin=8, rx_pin=9)
     esp01_mqtt.setup(
@@ -32,16 +38,20 @@ def loop():
         if new_flow_rate:
             sensor['flow_rate']=new_flow_rate
         sensor['water_level']=round(read_water_level(),2)
+        print("Pressure = {} hpa".format(sensor['pressure']))
+        print("Flow Rate = {} Litres/Hour".format(sensor['flow_rate']))
+        print("Water height = {} %".format(sensor['water_level']))
         
         x=str("Pressure = {} hpa".format(sensor['pressure']))   #print(flow_rate)
-        y=("Flow Rate = {} Litres/Hour".format(sensor['flow_rate']))
-        z=("Water height = {} %".format(sensor['water_level']))
+        y=str("Flow Rate = {} Litres/Hour".format(sensor['flow_rate']))
+        z=str("Water height = {} %".format(sensor['water_level']))
         time.sleep(0.5)
         value1_to_send = x
         value2_to_send = z
         value3_to_send = y
         esp01_mqtt.publish_data(value1_to_send, value2_to_send, value3_to_send)
         
+        wifi_module.send_data(x,y,z)
         
         
 
